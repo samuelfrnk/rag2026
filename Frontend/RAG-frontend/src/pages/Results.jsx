b7ed9304e9071a4aacd8dd7681551d043bdb2046
Ishana <ishana.rana@uzh.ch>
2026-04-26 19:21:34 +0200

    added Filter option

--- Frontend/RAG-frontend/src/pages/Results.jsx
+++ Frontend/RAG-frontend/src/pages/Results.jsx
@@ -2,9 +2,33 @@ import ProgressBar from "../components/ProgressBar/ProgressBar";
 import Sidebar from "../components/Sidebar/Sidebar";
 import ResultCard from "../components/Results/ResultCard";
 import "./Results.css";
+import { useState } from "react";
+import Filters from "../components/Filters/Filters";
+
 
 export default function Results({ papers, loading, error, total, keywords, text, file }) {
   if (error) return <p style={{ color: "red" }}>{error}</p>;
+  
+  const [sortBy, setSortBy] = useState({
+    type: "score",   // "score" | "year"
+    order: "desc"    // "asc" | "desc"
+  });
+
+  const sortedPapers = [...papers].sort((a, b) => {
+    if (sortBy.type === "score") {
+      return sortBy.order === "desc"
+        ? b.score - a.score
+        : a.score - b.score;
+    }
+
+    if (sortBy.type === "year") {
+      return sortBy.order === "desc"
+        ? b.year - a.year
+        : a.year - b.year;
+    }
+
+    return 0;
+  });
 
   return (
     <div className="results-layout">
@@ -21,21 +45,32 @@ export default function Results({ papers, loading, error, total, keywords, text,
       {/* RIGHT */}
       <div className="results-main">
 
-        <h2 className="title">Top {total} Matches</h2>
+        <h2 className="title">Yo Top {total} Matches</h2>
 
         {loading && <ProgressBar total={total} />}
 
+        <div className="disclaimer">
+          <p className="disclaimer-text">
+            The score is based on cosine similarity.
+            Please review the papers for relevance by simply clicking on it.
+          </p>
+        </div>
 
+        {/* <Filters /> */}
+        <Filters sortBy={sortBy} setSortBy={setSortBy} />
+
+
+
+        {/*<ResultsList papers={papers} loading={loading} error={error} />*/}
         <div className="result-cards">
           {!loading &&
-            papers.map((paper, i) => (
+            sortedPapers.map((paper, i) => (
               <ResultCard key={paper.id || i} paper={paper} index={i} />
             ))}
         </div>
 
-        {/* <Filters /> */}
+        
 
-        {/*<ResultsList papers={papers} loading={loading} error={error} />*/}
     
       </div>
 
